081d6b99ca9b96fa13f863d8c062fc9a6c720fa8
Hamoun Mojib <hamoun.mojib@pnmac.com>
2019-12-03 11:00:23 -0800

    Init

---
+++ src/DayThree/PartOne.js
@@ -0,0 +1,11 @@
+function getDirectionAndMovement(instruction) {
+  const direction = instruction.substring(0, 1);
+  const movement = instruction.substring(1);
+  return { direction, movement };
+}
+
+function PartOne(wireOne, wireTwo) {
+  let origin = { x: 0, y: 0 };
+}
+
+export default PartOne;
