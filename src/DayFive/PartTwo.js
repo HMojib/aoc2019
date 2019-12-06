1ed556196d0328db8648f2ec6d96e70cf290e680
Hamoun Mojib <hamoun.mojib@pnmac.com>
2019-12-06 11:28:20 -0800

    Started Part Two Day 5

---
+++ src/DayFive/PartTwo.js
@@ -0,0 +1,15 @@
+class Computer {
+  constructor(instructions, input) {
+    this.pointer = 0;
+    this.instructions = instructions;
+    this.input = input;
+  }
+
+  computeParameterModes(opcode) {
+    let stringOpcode = opcode.toString();
+    let parameterModes = []
+    if (stringOpcode.length > 2) {
+      const opcode = 
+    }
+  }
+}
